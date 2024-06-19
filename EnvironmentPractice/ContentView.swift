da492600374d6639758052f4eb148ede8c0ce9a7
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 14:36:56 +0900

    Completed Project.

--- EnvironmentPractice/ContentView.swift
+++ EnvironmentPractice/ContentView.swift
@@ -6,19 +6,56 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class AppState {
+  var isOn: Bool = false
+}
+
+
+struct LightBulbView: View {
+  @Environment(AppState.self) private var appState: AppState
+
+  var body: some View {
+    
+    @Bindable var appState = appState
+    Image(systemName:  appState.isOn ? "lightbulb.fill" : "lightbulb")
+      .font(.largeTitle)
+      .foregroundStyle(appState.isOn ? .yellow : .black)
+//    Button("Toggle"){
+//      appState.isOn.toggle()
+//    }
+    
+    Toggle("IsOn", isOn: $appState.isOn)
+  }
+  
+}
+
+
+struct LightRoomView: View {
+  var body: some View {
+    LightBulbView()
+  }
+}
+
+
+
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @Environment(AppState.self) private var appState: AppState
+  var body: some View {
+    VStack {
+      LightBulbView()
     }
+    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(appState.isOn ? .black : .white)
+  }
 }
 
 #Preview {
     ContentView()
+    .environment(AppState())
 }
