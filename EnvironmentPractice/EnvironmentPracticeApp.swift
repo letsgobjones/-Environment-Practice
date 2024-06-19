cb1408a8cef3f2d97ba49b3c52f0fd1bfe21dc9e
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 14:17:00 +0900

    Initial Commit

---
+++ EnvironmentPractice/EnvironmentPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentPracticeApp.swift
+//  EnvironmentPractice
+//
+//  Created by Brandon Jones on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
